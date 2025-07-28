9c0417888b47f6b1d50159cbec52861fc5b37f95
Abhinav Sharma <abhinavsharma9773@gmail.com>
2025-07-28 11:44:27 +0530

    Initial commit

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ import fetch from "node-fetch";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-
 app.use(cors());
 
 app.get("/weather", async (req, res) => {
@@ -40,6 +38,4 @@ app.get("/weather", async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`✅ Server running on http://localhost:${PORT}`);
-});
+export default app;
